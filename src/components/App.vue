6d64070554e07f39a81bdbc169fb89ae528473dc
Jaime <jaimedmiguel@gmail.com>
2018-03-02 14:35:11 +0100

    Estructura de proyecto creada

---
+++ src/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="wrapper">
+  </div>
+</template>
+
+<script>
+    export default {
+        name:'app'
+    }
+</script>
+
+<style lang="scss">
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['env']
+          //presets: ['es2015']
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader?name=/assets/img/[name].[ext]'
+      },
+      {
+        test: /\.(eot|ttf|woff)$/,
+        loader: 'file-loader?name=/assets/fonts/[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Codenotch',
+        filename: './index.html',
+        hash: true,
+        template: './template-app.ejs'
+    })
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map';
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
