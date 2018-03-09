8ef52056dd821911b671d04979ec6ffc8bf29863
Jaime <jaimedmiguel@gmail.com>
2018-03-09 12:54:57 +0100

    Estilos de encabezados h2, h3 y h4

--- src/components/platform/programa/ProgramaContenido.vue
+++ src/components/platform/programa/ProgramaContenido.vue
@@ -3,7 +3,7 @@
         <div class="container-fluid">
              <div class="row">
                 <div class="col-12 col-sm-10 offset-sm-1 col-xl-8 offset-xl-2">
-                    <h2>INTRODUCCIÓN AL FUNCIONAMIENTO DE INTERNET</h2>
+                    <h2>Introducción al funcionamiento de Internet</h2>
                     <img :src="setPathImgContenidos1" alt="img contenidos">
                     <p>Internet es una red mundial que para permite la conexión entre ordenadores a través de un protocolo de comunicaciones, formando <strong>una telaraña de información</strong>.</p>
                     <p>Para entender el funcionamiento de internet. hay que tener claros una serie de conceptos:</p>
@@ -16,7 +16,8 @@
                     <img :src="setPathImgContenidos2" alt="img contenidos">
                     <p>Esta petición, que hace el ordenador cliente al servidor cuando se escribe una dirección URL en la barra de direcciones del navegador web, se lleva a cabo a través del protocolo http (una serie de normas que se usa para acceder a páginas web).</p>
                     <p>Todos los ordenadores conectados a internet (tanto los ordenadores clientes como los servidores) tienen un identificador único (dirección IP). Para poder acceder desde un navegador web a un servidor web, necesitamos conocer la dirección IP del servidor web. Para facilitar esta tarea, está el sistema de nombres de dominio (DNS) que se encarga de traducir las direcciones IP a un nombre identificativo o Dominio.</p>
-                    <p>El servidor nos da una respuesta (a la petición solicitada) en forma de archivos HTML, que el navegador entiende e interpreta, como una página web./p>
+                    <p>El servidor nos da una respuesta (a la petición solicitada) en forma de archivos HTML, que el navegador entiende e interpreta, como una página web.</p>
+                    <h3>hosting</h3>
                     <p>Para poder acceder directamente a los archivos alojados en un hosting, tenemos el protocolo FTP, que permite transferir datos entre el ordenador cliente y el hosting a través de programas o clientes FTP como Filezilla, Ciberduck o Transmit.</p>
                 </div>
             </div>
