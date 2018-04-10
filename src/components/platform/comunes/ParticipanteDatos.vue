36db775e3c1a6f58510c4e3d9ddcfb042566d246
Jaime <jaimedmiguel@gmail.com>
2018-04-10 15:38:54 +0200

    Fix ParticipanteDatos

--- src/components/platform/comunes/ParticipanteDatos.vue
+++ src/components/platform/comunes/ParticipanteDatos.vue
@@ -13,9 +13,7 @@
             </div>
             <div class="row">
                 <div class="col-12 col-md-10 offset-md-1 col-lg-10 offset-lg-1 col-xl-6 offset-xl-3 container-data-participant">
-                    <!--dato-participante v-if="isContabilityProfile" v-for="(data,index) of currentParticipantData.data" :key="index" :data="currentParticipantData.data[index]"></dato-participante>
-                    <dato-participante v-else v-for="(data,index) of currentFilteredStudentData" :key="index" :data="currentFilteredStudentData[index]"></dato-participante-->
-                    <dato-participante v-for="(data,index) of currentFilteredStudentData" :key="index" :data="currentFilteredStudentData[index]"></dato-participante>
+                    <dato-participante v-for="(data,index) of currentFilteredData" :key="index" :data="currentFilteredData[index]"></dato-participante>
                 </div> 
             </div>
         </div>
@@ -23,13 +21,13 @@
 </template>
 
 <script>
-import Axios from "axios";
-import _ from "lodash";
-import { mapGetters } from "vuex";
+import Axios from 'axios';
+import _ from 'lodash';
+import { mapGetters } from 'vuex';
 import DatoParticipante from '../comunes/DatoParticipante.vue';
 
 export default {
-  name: "participanteDatos",
+  name: 'participanteDatos',
 
   components: {
       datoParticipante: DatoParticipante
@@ -39,16 +37,17 @@ export default {
     return {
       currentBootcampData: [],
       currentParticipantData: {},
-      currentFilteredStudentData: {},
-      bootcampId: "",
-      id: ""
+      currentFilteredData: {},
+      bootcampId: '',
+      id: '',
+      participantType: ''
     };
   },
 
   computed: {
     ...mapGetters({
-      config: "getConfigData",
-      bootcampData: "getBootcampData",
+      config: 'getConfigData',
+      bootcampData: 'getBootcampData',
       profile:'getProfile'
     }),
 
@@ -79,18 +78,17 @@ export default {
     /* set current Participant and type of Participant */ 
 
     let participantsList = null;
-    let participantType = '';
 
     switch (this.$route.name) {
       case "alumnoDatosAdmisiones":
       case "alumnoDatosContabilidad":
         participantsList = arrBootcampsData[indexBootcampMatched].studentList;
-        participantType = 'student';
+        this.participantType = 'student';
         break;
       case "profesorDatosAdmisiones":
       case "profesorDatosContabilidad":
         participantsList = arrBootcampsData[indexBootcampMatched].teacherList;
-        participantType = 'teacher';
+        this.participantType = 'teacher';
         break;
     }
     
@@ -102,7 +100,7 @@ export default {
 
      /* get filtered data by keys */ 
 
-    if(participantType === 'student'){
+    if(this.participantType === 'student'){
         let studentData = participantsList[indexParticipantsMatched].data;
         let firstFilterKey = 'shared';
         let secondFilterKey = this.profile;
@@ -110,8 +108,10 @@ export default {
 
         objFiltered = _.merge(_.pickBy(studentData, item => item.type === firstFilterKey),_.pickBy(studentData, item => item.type === secondFilterKey));
 
-        this.currentFilteredStudentData = objFiltered;
-    }
+        this.currentFilteredData = objFiltered;
+    } 
+
+    else this.currentFilteredData = this.currentParticipantData.data;
   }
 };
 </script>
